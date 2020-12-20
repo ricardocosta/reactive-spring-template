650de47c33acce50f6eca23a0f4c17faab12d9e5
Ricardo Costa <ricardocosta1989@gmail.com>
2020-12-20 19:20:10 +0000

    feat: Add support for Pagination and Sorting
    Add the necessary configuration to automatically create instances of
    'Sort' and 'Pageable' based on query parameters.

--- src/main/kotlin/com/ricardocosta/template/configuration/WebConfig.kt
+++ src/main/kotlin/com/ricardocosta/template/configuration/WebConfig.kt
@@ -1,9 +1,12 @@
 package com.ricardocosta.template.configuration
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.web.ReactivePageableHandlerMethodArgumentResolver
+import org.springframework.data.web.ReactiveSortHandlerMethodArgumentResolver
 import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.PathMatchConfigurer
 import org.springframework.web.reactive.config.WebFluxConfigurer
+import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer
 
 @Configuration
 @EnableWebFlux
@@ -12,5 +15,12 @@ class WebConfig : WebFluxConfigurer {
         configurer
             .setUseCaseSensitiveMatch(true)
             .setUseTrailingSlashMatch(false)
+    }
+
+    override fun configureArgumentResolvers(configurer: ArgumentResolverConfigurer) {
+        configurer.addCustomResolver(
+            ReactiveSortHandlerMethodArgumentResolver(),
+            ReactivePageableHandlerMethodArgumentResolver()
+        )
     }
 }
